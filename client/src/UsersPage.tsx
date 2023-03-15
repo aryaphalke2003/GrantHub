c930701af5af8cd4dc4f9c9120d3a9f1103098b9
aaka-123 <92880311+aaka-123@users.noreply.github.com>
2023-03-15 10:56:16 +0530

    Update UsersPage.tsx

--- client/src/UsersPage.tsx
+++ client/src/UsersPage.tsx
@@ -41,6 +41,7 @@ export default function UsersPage() {
                         <AccountForm onSubmit={() => toggleRefetchTrigger()} />
                     </Collapse>
                 </MyCard>
+                
             </div>
             
         </PageContainer>
